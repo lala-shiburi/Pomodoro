dfef3202d6bb604d1972033b26009d4fca0acda0
Nhlalala <chanellesesh@gmail.com>
2019-05-14 14:48:36 +0200

    Adds 60 minutes limit for both breaks and sessions

--- src/components/Controllers.jsx
+++ src/components/Controllers.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Label from './Label'
+import Label from "./Label";
 import BreakIncrement from "./BreakIncrement";
 import SessionIncrement from "./SessionIncrement";
 import BreakDecrement from "./BreakDecrement";
@@ -9,70 +9,65 @@ import Reset from "./Reset";
 
 export default class Controllers extends Component {
   state = {
-    breakLength: 5.00,
-    sessionLength: 25.00,
-    currentTimer:1500,
-    timerType:'session',
-    timerState:'start',
+    breakLength: 5.0,
+    sessionLength: 25.0,
+    currentTimer: 1500,
+    timerType: "session",
+    timerState: "start"
   };
 
   render() {
-    
-
     const handleClickIncrement = e => {
       let controllerID = e.target.id;
-      if (controllerID === "break-increment") {
+      if (controllerID === "break-increment" && this.state.breakLength<=59) {
         this.setState({ breakLength: this.state.breakLength + 1 });
-      } else {
+      } else if(controllerID === "session-increment" && this.state.sessionLength<=59) {
         this.setState({ sessionLength: this.state.sessionLength + 1 });
         this.setState({ currentTimer: this.state.currentTimer + 60 });
       }
     };
 
-    const clockDisplay=()=>{
-      let minutes=Math.floor(this.state.currentTimer/60);
-      let seconds=this.state.currentTimer-minutes*60;
-      seconds = seconds < 10 ? '0' + seconds : seconds;
-      minutes = minutes < 10 ? '0' + minutes : minutes;
-      return minutes + ':' + seconds;
-    }
+    const clockDisplay = () => {
+      let minutes = Math.floor(this.state.currentTimer / 60);
+      let seconds = this.state.currentTimer - minutes * 60;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      return minutes + ":" + seconds;
+    };
 
-    const reset=()=>{
-      let intervalButton= document.getElementById('start_stop');
+    const reset = () => {
+      let intervalButton = document.getElementById("start_stop");
       console.log(intervalButton);
       clearInterval(intervalButton.interval);
-        
-      this.setState({breakLength: 5.00,
-                      sessionLength: 25.00,
-                      currentTimer:1500,
-                      timerType:'session',
-                      timerState:'start',})
-                    }
 
-    const handleClickStartStop=(event)=>{
-      
-      if(event.target.interval && this.state.timerState === 'stop' ){
+      this.setState({
+        breakLength: 5.0,
+        sessionLength: 25.0,
+        currentTimer: 1500,
+        timerType: "session",
+        timerState: "start"
+      });
+    };
+
+    const handleClickStartStop = event => {
+      if (event.target.interval && this.state.timerState === "stop") {
         clearInterval(event.interval);
-        this.setState({timerState:'start'})
-        
-    }else{
-      
-      let count=()=>{
-        this.setState({currentTimer:this.state.currentTimer-1})
+        this.setState({ timerState: "start" });
+      } else {
+        let count = () => {
+          this.setState({ currentTimer: this.state.currentTimer - 1 });
+        };
+        event.target.interval = setInterval(count, 1000);
+        this.setState({ timerState: "stop" });
       }
-       event.target.interval=setInterval(count,1000)
-       this.setState({timerState:'stop'})
-    }
-          
-    }
+    };
 
-
-     const handleClickDecrement=event => {
+    const handleClickDecrement = event => {
       let controllerID = event.target.id;
-      if(controllerID === 'break-decrement'){
-        this.setState({breakLength:this.state.breakLength-1})
-      }else{
-        this.setState({sessionLength:this.state.sessionLength-1})
+      if (controllerID === "break-decrement" && this.state.breakLength>=2) {
+        this.setState({ breakLength: this.state.breakLength - 1 });
+      } else if(controllerID === 'session-decrement' && this.state.sessionLength>=2){
+        this.setState({ sessionLength: this.state.sessionLength - 1 });
         this.setState({ currentTimer: this.state.currentTimer - 60 });
       }
     };
